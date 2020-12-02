d111e2bd5ddc381e68bcf94871f0c30a7fc90f74
miksa599 <miksa599@olympen1-106.ad.liu.se>
2020-12-02 17:19:40 +0100

    undo

--- Lab3/average.cpp
+++ Lab3/average.cpp
@@ -103,12 +103,12 @@ int main(int argc, char* argv[])
 	// and conv.setOverlap(<integer>)
 	{
 		auto conv = skepu::MapOverlap(average_kernel_1d);
-		conv.setOverlapMode(skepu::Overlap::RowWise);
+		conv.setOverlapMode(skepu::Overlap::ColWise);
 		conv.setOverlap(radius*imageInfo.elementsPerPixel);
 		auto timeTaken = skepu::benchmark::measureExecTime([&]
 		{
 			conv(tmp, inputMatrix, imageInfo.elementsPerPixel);
-			conv.setOverlapMode(skepu::Overlap::ColWise);
+			conv.setOverlapMode(skepu::Overlap::RowWise);
 			conv.setOverlap(radius);
 			conv(outputMatrixSep, tmp, 1);
 		});
